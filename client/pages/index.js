75fdc31c871cf7faba452ad8e6730334c8576a12
Jamil Madison <contact@jamilmadison.com>
2024-03-03 18:39:00 -0800

    Added client directory & installed NextJS

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage!</h1>
+            <p>This is the default homepage of your website.</p>
+        </div>
+    );
+};
+
+export default HomePage;
