1c4c649319c4261dcd71b7efabeb8e0a583d9b6c
Jamil Madison <contact@jamilmadison.com>
2024-03-05 01:44:46 -0600

    Added form and basic functionality

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SellerForm from "../components/SellerForm";
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1>Welcome to the Homepage!</h1>
+		<div className='container mx-auto px-4'>
+			<h1 className='text-xl font-bold my-4'>Register Seller</h1>
 			<p>This is the default homepage of your website.</p>
+			<SellerForm />
 		</div>
 	);
 };
