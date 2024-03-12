eafdfbc074bb5d01f3f6b6f3e1c223654f630e6d
Jesstin Swadley <contact@jesstin.com>
2024-03-12 15:30:40 -0700

    password hashing and jwt implementation

--- server/src/controllers/merchant.controller.js
+++ server/src/controllers/merchant.controller.js
@@ -1,35 +1,67 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {
 	models: { Merchant },
 } = require("../models/db");
 
+
 const registerMerchant = async (req, res) => {
-	let email = req.body.email;
-	let password = req.body.password;
+	const saltRounds = 10;
+	const email = req.body.email;
+	const password = req.body.password;
 
 	try {
+		const hashPassword = await bcrypt.hash(password, saltRounds);
+
 		const merchant = await Merchant.create({
 			email,
-			password,
+			hashPassword
 		});
 
-		return res.send(merchant);
+		const token = jwt.sign(
+			{ 
+				data: merchant 
+			}, 
+			privateKey,
+			{ 
+				expiresIn: '1h' 
+			}
+		);
+
+		return res.send(token);
 	} catch (error) {
 		return res.send(error);
 	}
 };
 
 const loginMerchant = async (req, res) => {
-	let email = req.body.email;
+	const email = req.body.email;
+	const merchantPassword = req.body.password
+
 	try {
 		const merchant = await Merchant.findOne({
 			where: { email },
 		});
 
-		if (!merchant) {
+		const match = await bcrypt.compare(merchantPassword, merchant.password);
+
+		if (!merchant || !match) {
 			return res.send("Incorrect Email or Password");
 		}
-		console.log("Merchant found, ID:", merchant.id);
-		return res.json({ message: "Welcome to the server", id: merchant.id });
+
+		const token = jwt.sign(
+			{ 
+				data: {
+					merchantId: merchant.id
+				}
+			}, 
+			privateKey,
+			{ 
+				expiresIn: '1h' 
+			}
+		);
+
+		return res.json(token);
 	} catch (error) {
 		return res.send(error);
 	}
