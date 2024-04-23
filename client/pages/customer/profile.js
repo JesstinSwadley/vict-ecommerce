ef7ede53e65efd7e66afaf21f033d1beae9e96e6
Jamil Madison <contact@jamilmadison.com>
2024-04-23 04:29:46 -0700

    Add customer profile page

---
+++ client/pages/customer/profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TransactionTable from '../../components/TransactionTable';
+
+export default function Profile() {
+	return (
+		<div>
+			<h1>Profile</h1>
+			<TransactionTable />
+		</div>
+	);
+}
